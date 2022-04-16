9de21b528fb891aab95afd782eb8fdacee5acddc
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-04-16 15:07:22 +0200

    created props and propTypes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,27 @@ import './App.css';
 import data from './data';
 import AnimalCard from '../AnimalCard/AnimalCard';
 
+const showAdditional = (additional) => {
+  const alertInformation = Object.entries(additional)
+    .map(information => `${information[0]}: ${information[1]}`)
+    .join('\n'); //join converts array to string
+  alert(alertInformation)
+};
+
 function App() {
   return (
     <div className ="wrapper">
       <h1>Animals</h1>
       {data.map(animal => (
         <AnimalCard
-        
+        additional={animal.additional}
+        diet={animal.diet}
         key={animal.name}
-        name={animal.name}/>
+        name={animal.name}
+        scientificName={animal.scientificName}
+        showAdditional={showAdditional}
+        size={animal.size}
+        />
       ))}
     </div>
   )
